43c950c5a9d9b7272527fad4590db7a16e3b2125
David Jung <david.jung@zollsoft.de>
2026-03-09 08:04:57 +0100

    Version number bumped to 2.1.6

--- sql/upgrade.sql
+++ sql/upgrade.sql
@@ -41,6 +41,8 @@ CREATE EXTENSION bdr VERSION '2.1.4.0';
 DROP EXTENSION bdr;
 CREATE EXTENSION bdr VERSION '2.1.5.0';
 DROP EXTENSION bdr;
+CREATE EXTENSION bdr VERSION '2.1.6.0';
+DROP EXTENSION bdr;
 
 -- evolve version one by one from the oldest to the newest one
 CREATE EXTENSION bdr VERSION '1.0.0.0';
@@ -59,6 +61,7 @@ ALTER EXTENSION bdr UPDATE TO '2.1.2.0';
 ALTER EXTENSION bdr UPDATE TO '2.1.3.0';
 ALTER EXTENSION bdr UPDATE TO '2.1.4.0';
 ALTER EXTENSION bdr UPDATE TO '2.1.5.0';
+ALTER EXTENSION bdr UPDATE TO '2.1.6.0';
 
 -- Should never have to do anything: You missed adding the new version above.
 ALTER EXTENSION bdr UPDATE;
